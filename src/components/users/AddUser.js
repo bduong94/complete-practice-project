1b61bd7754821ea0b602f02b11497c13bb50ac7e
Brian Duong <brianminhduong@gmail.com>
2022-06-22 21:26:33 -0400

    Add CSS style through Style Modules

--- src/components/users/AddUser.js
+++ src/components/users/AddUser.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
   const addUserHandler = (e) => {
@@ -8,7 +9,7 @@ const AddUser = (props) => {
 
   return (
     <Card>
-      <form onSubmit={addUserHandler}>
+      <form className={classes.input} onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
         <input id="username" type="text" />
         <label htmlFor="age">Age (Years)</label>
